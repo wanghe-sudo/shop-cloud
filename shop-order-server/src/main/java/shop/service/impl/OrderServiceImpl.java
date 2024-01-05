ec69800f88f2c1153eb32007ba8734dce7088e07
wanghe <wanghe-sudo@sudo.com>
2024-01-06 00:14:23 +0800

    add:使用DiscoveryClient查看已经注册的服务

--- shop-order-server/src/main/java/shop/service/impl/OrderServiceImpl.java
+++ shop-order-server/src/main/java/shop/service/impl/OrderServiceImpl.java
@@ -2,6 +2,8 @@ package shop.service.impl;
 
 import com.alibaba.fastjson2.JSON;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
@@ -11,6 +13,7 @@ import shop.shop.domain.Order;
 import shop.shop.domain.Product;
 
 import javax.annotation.Resource;
+import java.util.List;
 
 @Service
 @Log4j2
@@ -18,14 +21,22 @@ public class OrderServiceImpl implements OrderService {
     @Resource
     private OrderDao orderDao;
     @Resource
+    private DiscoveryClient discoveryClient;
+    @Resource
     private RestTemplate restTemplate;
 
     @Override
     public Order createOrder(Long productId, Long userId) {
         Product product = null;
         // 使用feign最为远程调用工具(可以使用http请求发送)
-        ResponseEntity<String> forEntity = restTemplate.getForEntity("http://127.0.0.1:8081/product/" + productId,
-                String.class);
+        ResponseEntity<String> forEntity = restTemplate.getForEntity("http://127.0.0.1:8081/product/" + productId, String.class);
+        log.info("1 ===> ", JSON.toJSONString(forEntity.getBody()));
+        List<ServiceInstance> serverList = discoveryClient.getInstances("product-service");
+
+        String host = serverList.get(0).getHost();
+        int port = serverList.get(0).getPort();
+        forEntity = restTemplate.getForEntity("http://" + host + ":" + port + "/product/" + productId, String.class);
+        log.info("2 ===> ", JSON.toJSONString(forEntity.getBody()));
         product = JSON.parseObject(forEntity.getBody(), Product.class);
         Order order = Order.builder().uid(userId).pid(productId)
                 .pname(product.getPname())
